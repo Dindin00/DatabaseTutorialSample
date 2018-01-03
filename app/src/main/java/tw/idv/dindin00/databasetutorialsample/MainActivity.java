fcdd3b2bbe94a4dfb32ebf01087334260049fe1d
LI-ZHI_CHEN <Dindin5200@gmail.com>
2018-01-03 23:08:46 +0800

    UI元件放置及變數宣告

--- app/src/main/java/tw/idv/dindin00/databasetutorialsample/MainActivity.java
+++ app/src/main/java/tw/idv/dindin00/databasetutorialsample/MainActivity.java
@@ -1,9 +1,16 @@
 package tw.idv.dindin00.databasetutorialsample;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //用來顯示結果的ListView宣告
+    ListView lv_show;
+    //SQLite資料庫變數宣告
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
