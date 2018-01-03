f6882dc48ec436e334f93611475ba5e595280208
LI-ZHI_CHEN <Dindin5200@gmail.com>
2018-01-03 23:58:28 +0800

    查詢資料表的內容並顯示在ListView上

--- app/src/main/java/tw/idv/dindin00/databasetutorialsample/MainActivity.java
+++ app/src/main/java/tw/idv/dindin00/databasetutorialsample/MainActivity.java
@@ -1,9 +1,11 @@
 package tw.idv.dindin00.databasetutorialsample;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +33,26 @@ public class MainActivity extends AppCompatActivity {
         database.execSQL("insert into students values(10510108, '王小明', '資二A')");
         database.execSQL("insert into students values(10510215, '鄭成功', '資二B')");
         database.execSQL("insert into students values(10410345, '陳筱玲', '資三C')");
+
+        //透過rawQuery執行SQL的select語法查詢資料
+        //再利用Cursor儲存查詢出來的結果
+        //第一個參數為查詢的SQL語法
+        //第二個參數是如果要透過android動態傳入參數至SQL與法中所需要使用的參數，若無傳入則填null即可
+        //若有要傳入參數則在SQL語法中輸入?，再依序傳入即可如下所示
+        // Cursor cursor = database.rawQuery("select * from ?", new String[]{"students"});
+        Cursor cursor = database.rawQuery("select * from students", null);
+        //將查詢結果(cursor)放入Adapter中用以放入ListView顯示
+        //第一個參數為要顯示資料的activity的context
+        //第二個參數為ListView中每一列要套用的樣式，本範例採用android預設的樣式，也可自行設計layout
+        //第三個參數為要用來顯示的SQL查詢結果
+        //第四個參數為要顯示的欄位名稱
+        //第五個參數為要在每一列的哪個預設位置顯示，若自行設定layout則採用R.id.xxx即可
+        //第六個參數用於監聽特殊處理之用，若不需使用輸入0即可
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(MainActivity.this, android.R.layout.simple_list_item_1, cursor, new String[]{"name", "class"}, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        //ListView初始化
+        lv_show = (ListView) findViewById(R.id.lv_show);
+        //設定ListView的資料內容(Adapter)
+        lv_show.setAdapter(cursorAdapter);
 
     }
 }
